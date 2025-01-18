93b7c110389dabe340864448b0ffc77002c37e5e
Welat-E <welaterenug@gmail.com>
2025-01-18 17:25:36 +0100

    Creating first endpoint

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def get_characters():
+    with open(data/characters.json, 'r') as file:
+        for character in file:
+            print(character["name"])
+
