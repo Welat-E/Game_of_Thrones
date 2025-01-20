05c16102d46c85535393a45f072c4fd6ea127fb8
Welat-E <welaterenug@gmail.com>
2025-01-20 19:43:23 +0100

    Fixing 1. endpoint

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def get_characters():
-    with open(data/characters.json, 'r') as file:
-        for character in file:
+    with open("data/characters.json", "r") as file:
+        data = json.load(file)  # loading the json file in a object
+        for character in data:
             print(character["name"])
 
+get_characters()
