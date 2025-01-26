6ea5f9291cac3813943929fad617009698a78c0f
Welat-E <welaterenug@gmail.com>
2025-01-26 13:24:39 +0100

    Fixing first endpoint get all characters

--- app.py
+++ app.py
@@ -6,34 +6,31 @@ from operator import itemgetter
 app = Flask(__name__)
 
 
-# @app.route("/home", methods=["GET"])
-# def get_characters():
-#     while True:
-#         try:
-#             limit = input("How many Characters do you wanna see? ")
-#             skip = input("Where should the List of Characters start? ")
-#             if limit == "" and skip == "":
-#                 limit = int(20)
-#                 skip = int(0)
-#                 break
-#             elif limit != "" and skip != "":
-#                 limit = int(limit)
-#                 skip = int(skip)
-#                 break
-#             else:
-#                 raise Exception
-#         except:
-#             print("Both must be filled with integer or should be empty.")
-#     with open("data/characters.json", "r") as file:
-#         data = json.load(file)  # loading the json file in a object
-#         if limit == int(20) and skip == int(0):
-#             random.shuffle(data)
-#         for character in data[skip : limit + skip]:  # we iterate over the list and go,
-#             print()  # in each selected dict. and print each key and value inside.
-#             for key, value in character.items():
-#                 print(f"{key.title()}: {value}")
-# get_characters()
-
+@app.route("/get_all_characters", methods=["GET"])
+def get_all_characters():
+    get_characters_lst = []
+    try:
+        match = False
+        limit = request.args.get("limit")
+        skip = request.args.get("skip")
+        if limit == "" and skip == "":
+            limit = int(20)
+            skip = int(0)
+            match = True
+        elif limit != "" and skip != "":
+            limit = int(limit)
+            skip = int(skip)
+        else:
+            raise Exception
+    except:
+        return jsonify("Both must be filled with integer or should be empty.")
+    with open("data/characters.json", "r") as file:
+        data = json.load(file)  # loading the json file in a object
+        if match: #match == True means in this case that there no user entry for skip and limit because we defined it on line 16-19 
+            random.shuffle(data)  #it shuffles the data
+        for character in data[skip : limit + skip]:  # we iterate over the list
+            get_characters_lst.append(character)
+    return jsonify(get_characters_lst)
 
 @app.route("/get_character_by_id", methods=["GET"])
 def get_character_by_id():
